745422a0035b62e1feeffa9266edffa67f05d7bb
Nir Levi <n1rlevin1r100@gmail.com>
2022-12-13 20:24:52 +0200

    Server Func Email

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+//הפעלת פונקציה מתוך ספריית אי אן וי שטוענת את ההגדרות מקובץ דוט אי אן וי
+require('dotenv').config();
+
+function SendEmail(u,p) {
+  // קרא לגוגל והתחבר באמצעת השם והסיסמה
+  console.log("Sent Email to" + u );
+}
+  // process זה אובייקט שמייצג את האפליקציה שלי
+  SendEmail(process.env.USER_EMAIL,process.env.EMAIL_PASS)
 // קישור לספריית פרוטוקול עבודה באינטרנט
 const http = require('http');
 // קישור לאפליקציה שכתבנו בקובץ החיצוני
